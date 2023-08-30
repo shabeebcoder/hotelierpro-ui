de58f55a0022c055f6b39d80b6b09c8928545f7c
hotelierpro123 <hotelierpro123@gmail.com>
2023-08-30 20:28:14 +0400

    fixed

--- src/Buttons/Button.stories.tsx
+++ src/Buttons/Button.stories.tsx
@@ -11,13 +11,14 @@ Primary.args = {
     children: "Primary Button",
     type: 'default',
     disabled: false,
-    style: { backgroundColor: '#3A7jC7C', color: 'white', }
+    style: { backgroundColor: '#3A7C7C', color: 'white', }
 }
 export const Default = (args) => <Buttons {...args} />
+
 Default.args = {
     children: "Primary Button",
     type: 'default',
-    style: {backgroundColor:'#3A7C7C', color:'white'}
+    style: { backgroundColor: '#3A7C7C', color: 'white' }
 
 }
 

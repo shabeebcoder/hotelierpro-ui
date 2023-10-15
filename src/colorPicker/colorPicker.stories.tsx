f6949a89a96b673729f826ce4df93e911938925b
hotelierpro123 <hotelierpro123@gmail.com>
2023-10-15 11:37:20 +0545

    color picker compoenent initiated

---
+++ src/colorPicker/colorPicker.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ColorPicker from "./colorPicker";
+
+export default {
+    title: "components/colorPicker"
+}
+
+export const ColorPickers = (args) => <ColorPicker {...args} />
+
+
+
+ColorPickers.args = {
+
+}
---
+++ src/colorPicker/colorPicker.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles.css"
+
+function ColorPicker() {
+    return (
+        <div className='colorPickerContainer'>ColorPicker</div>
+    )
+}
+
+export default ColorPicker

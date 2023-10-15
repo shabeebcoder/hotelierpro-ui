ad900ed902734e3dd0e26711e74f773a81cce146
mynamealfasrahman@gmail.com <mynamealfasrahman@gmail.com>
2023-10-15 14:22:02 +0530

    pushed

--- src/colorPicker/colorPicker.tsx
+++ src/colorPicker/colorPicker.tsx
@@ -3,7 +3,15 @@ import "./styles.css"
 
 function ColorPicker() {
     return (
-        <div className='colorPickerContainer'>ColorPicker</div>
+        <div className='colorPickerContainer'>
+            <div className='color-picker-text-div'>
+                <h1 className='color-picker-text'>Background Color</h1>
+            </div>
+            <div className='color-picker-primary-box'>
+                <h3 className='color-picker-secondary-text'>#EDEDED</h3>
+                <div className='color-picker-tertiary-box'></div>
+            </div>
+        </div>
     )
 }
 

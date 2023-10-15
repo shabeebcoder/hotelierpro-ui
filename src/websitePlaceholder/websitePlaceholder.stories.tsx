c7d0bff55321f6187100dc94c0c967d77d42b34d
hotelierpro123 <hotelierpro123@gmail.com>
2023-10-15 15:20:42 +0545

    website placeholder function initiated

---
+++ src/websitePlaceholder/websitePlaceholder.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WebsitePlaceholder from "./websitePlaceholder";
+
+
+export default {
+    title: 'components/websitePlaceholder'
+}
+
+
+export const WebsitePlaceholders = (args) => <WebsitePlaceholder {...args} />
+
+WebsitePlaceholders.args = {
+
+}
---
+++ src/websitePlaceholder/websitePlaceholder.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles.css";
+
+function WebsitePlaceholder(props) {
+    return (
+        <div>WebsitePlaceholder</div>
+    )
+}
+
+export default WebsitePlaceholder

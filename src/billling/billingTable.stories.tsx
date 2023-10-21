f5d294dfc40708c3647adb12828d2608415cb82a
hotelierpro123 <hotelierpro123@gmail.com>
2023-10-21 14:08:11 +0545

    Billing Table implemented

---
+++ src/billling/billingTable.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BillingTables from "./billingTable";
+
+export default {
+    title: 'components/Billing'
+}
+
+
+export const BillingTable = (args) => <BillingTables {...args} />
+
+
+
+BillingTable.args = {}

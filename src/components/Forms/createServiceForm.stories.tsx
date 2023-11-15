08d8f51399fefbebeb5b4f1299627d915c4cccbd
shabeebcoder <shabeebcoder@gmail.com>
2023-11-15 21:34:59 +0400

    added service form

---
+++ src/components/Forms/createServiceForm.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CreateServiceForm from "./createServiceForm";
+
+export default {
+    title: "hotelierPro/Form/Create-service-form"
+}
+
+
+export const CreateServiceFormExample = (args) => <CreateServiceForm {...args} />
+
+
---
+++ src/components/Forms/createServiceForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateServiceForm() {
+  return (
+    <div>CreateServiceForm</div>
+  )
+}

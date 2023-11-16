9493c56e5258a009bddd5958cdaa034e4776a7dd
Ahammed Alfas <mynamealfasrahman@gmail.com>
2023-11-16 13:52:09 +0530

    check out this

--- src/components/Forms/roomtype/addroomType.tsx
+++ src/components/Forms/roomtype/addroomType.tsx
@@ -1,8 +1,68 @@
 import React from 'react'
-
+import {Input} from '../../../elements/Input/input'
+import {Label} from '../../../elements/Label/label'
+import {Button} from '../../../elements/Buttons/buttons'
+import { FormField ,FormItem ,FormLabel,FormControl,FormDescription,FormMessage ,Form} from '../../../elements/Form/form'
+import { useFieldArray, useForm } from "react-hook-form"
 function AddRoomType() {
+  const form = useForm({})
   return (
-    <div>AddRoomType</div>
+    <div>
+      <Form {...form}>
+          <form onSubmit={form.handleSubmit(() => alert())} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Max. Persons(per rooms)</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price(Regular use)</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price(Single use)</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Add</Button>&nbsp;&nbsp;&nbsp;
+            <Button variant="secondary" type="submit">Cancel</Button>
+          </form>
+        </Form>
+    </div>
   )
 }
 

5193fa8cedbcc36a007c1d8b6d4d1762b8630f98
shabeebcoder <shabeebcoder@gmail.com>
2023-11-16 10:36:40 +0400

    room add type added

---
+++ src/components/Forms/roomtype/addroomType.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import AddRoomType from "./addroomType";
+
+
+export default {
+    title : "hotelierPro/Form/addRoomType"
+}
+
+
+
+export const AddRoomTypeExample = (args) => <AddRoomType {...args} />
+AddRoomTypeExample.args = {}
---
+++ src/components/Forms/roomtype/addroomType.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddRoomType() {
+  return (
+    <div>AddRoomType</div>
+  )
+}
+
+export default AddRoomType

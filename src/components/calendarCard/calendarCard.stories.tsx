25abbce90307aaf22f6e7a63b6eebce07db5f355
Akshaypmna18 <akshaypmnacoc@gmail.com>
2023-11-28 19:36:04 +0530

    created calendarCard design

---
+++ src/components/calendarCard/calendarCard.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CalendarCard from './calendarCard';
+
+export default {
+    title: 'hotelierPro/CalendarCard',
+};
+
+export const CalendarCardExample = (args) => <CalendarCard {...args} />;
+
+CalendarCardExample.args = {
+    name: 'Sebastian',
+    bgColor: 'yellow-200',
+    textColor: 'yellow-600',
+    countColor: 'yellow-700',
+    iconColor: 'yellow-500',
+    height: '3rem',
+    width: '10rem',
+    userCount: '2',
+    nightsCount: '3',
+};
